23b94cf15b23bfa239f897fcee7083fd8f7cf96d
axxeeeelllllle <guardianracelle@gmail.com>
2026-06-17 13:04:38 +0800

    Add files via upload

---
+++ hellojava.java
@@ -0,0 +1,10 @@
+package Basic_structure_syntax;// this is a single java program that prints "I am racelle" to the console.
+public class hellojava {
+    public static void main(String[] args) {
+        System.out.println("I am racelle");
+        System.out.println("I am 17 years old");
+        System.out.println("I am a student of ICT program  ");
+        System.out.println("I am from the Philippines");
+        System.out.println("I am a Filipino");
+    }
+}
