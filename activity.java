4a61c151487d535c66a20adabfdf5b237155061c
axxeeeelllllle <guardianracelle@gmail.com>
2026-06-17 14:47:06 +0800

    Add files via upload

---
+++ activity.java
@@ -0,0 +1,29 @@
+;// this is a single java program that prints "I am racelle" to the console.
+public class activity {
+    public static void main(String[] args) {
+        System.out.println("I am racelle");
+        System.out.println("212345754882l");
+        System.out.println("I am a student of ICT program");
+        System.out.println("I am from the Philippines");
+        System.out.println("I am a Filipino");
+        System.out.println("Addition");
+        System.out.println(5+5);
+        System.out.println("Subtraction");
+        System.out.println(5-5);
+        System.out.println("Multiplication");
+        System.out.println(5*5);
+        System.out.println("Division");
+        System.out.println(5/5);
+    
+
+        //system -This is a standard Java class that contains tools for interacting with your computer.
+        //out - This is a standard output stream that is used to display text on the console.
+        //println - This is a method that prints text to the console and moves the cursor to the next line.
+        //String[] args - This is an array of strings that can be used to pass command-line arguments to the program.
+        //main - This is the entry point of the program, where the code execution starts.
+        //public - This is an access modifier that allows the class to be accessed from other classes.
+        
+    }
+}
+
+}
